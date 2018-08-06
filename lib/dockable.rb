66e270db0d56b665c7db3158fceb49f4b1b7f5d3
iain <iain@iain.nl>
2018-08-06 14:30:18 -0400

    Improvements for SSM support
    * Decrypt values
    * Allow multiple sets of responses
    * Convert non-word chars to underscores
    * Force param values to strings
    
    Sneaked in:
    
    * use `YAML.safe_load`
    * use Pry for debugging instead of IRB

--- lib/dockable.rb
+++ lib/dockable.rb
@@ -1,26 +1,17 @@
-require "dockable/version"
-
 require "yaml"
+
+require "aws-sdk-ssm"
+require "aws-sdk-s3"
+
+require "dockable/version"
 
 module Dockable
 
   def self.load_environment_variables(bucket:, key:, ssm_prefix:)
     if presence(ssm_prefix)
-      require "aws-sdk-ssm"
-      client     = Aws::SSM::Client.new
-      ssm_prefix = "/#{ssm_prefix}" if ssm_prefix[0] != "/" # get_parameters_by_path requires leading /
-      parameters = client.get_parameters_by_path(path: ssm_prefix).parameters
-      if parameters.count != 0
-        parameters.map{|param| [param.split("/").last.upcase, param.value]}.to_h
-      else
-        {}
-      end
+      load_from_ssm(ssm_prefix: ssm_prefix)
     elsif presence(bucket) && presence(key)
-      require "aws-sdk-s3"
-      client = Aws::S3::Client.new
-      resp   = client.get_object(bucket: bucket, key: key)
-      body   = resp.body.read
-      YAML.load(body).map { |k, v| [ k.to_s, v.to_s ] }.to_h
+      load_from_s3(bucket: bucket, key: key)
     else
       STDERR.puts "Dockable warning: No bucket/key or SSM Prefix found, no environment file loaded."
       {}
@@ -36,7 +27,7 @@ module Dockable
       else
         argv
       end
-    elsif argv.size == 0
+    elsif argv.empty?
       fail ArgumentError, "No command specified"
     else
       argv
@@ -57,6 +48,25 @@ module Dockable
     else
       value
     end
+  end
+
+  def self.load_from_ssm(ssm_prefix:)
+    client     = Aws::SSM::Client.new
+    ssm_prefix = "/#{ssm_prefix}" if ssm_prefix[0] != "/" # get_parameters_by_path requires leading /
+    parameters = client.get_parameters_by_path(path: ssm_prefix, with_decryption: true).each.flat_map(&:parameters)
+    parameters.map { |param|
+      [
+        param.name.split("/").last.upcase.gsub(/\W/, "_"),
+        param.value.to_s
+      ]
+    }.to_h
+  end
+
+  def self.load_from_s3(bucket:, key:)
+    client = Aws::S3::Client.new
+    resp   = client.get_object(bucket: bucket, key: key)
+    body   = resp.body.read
+    YAML.safe_load(body).map { |k, v| [ k.to_s, v.to_s ] }.to_h
   end
 
 end
